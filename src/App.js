29f8ddbec29504beba4769e08b03a04ef1144115
Jervin Francisco <fran0436@algonquinlive.com>
2018-10-03 14:29:36 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
 
-class App extends Component {
+export default class App extends React.Component {
+constructor(){
+  super();
+  //lifecycle method 1
+  // initial values for State
+  // state is whats tide to the component , state is like property
+
+  this.state = {
+    day: 'wednesday'
+  }
+
+}
+
+componentWillMount(){
+ // added just before the screen
+ // lifecycle method 2
+
+ console.log(this.state.day)
+}
+
+componentDidMount(){
+// after your stuff is on the screen
+// lifecycle mthod 4
+
+// fetch calls made here
+
+console.log(this.state.day)
+
+}
+
   render() {
+    // life cycle mthod 3
+    // Do not change state here
+    console.log(this.state.day)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      
+      <React.Fragment>
+
+        <Header />
+        <main>
+          <p>Inside the frag</p>
+        </main>
+
+      </React.Fragment>
     );
   }
 }
 
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <header className = "app-header">
+            <h1>YOOOOOOOOOOOOOOOO</h1>
+            </header>
+        )
+    }
+}
